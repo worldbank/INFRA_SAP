a2644bde39516d557543b96216f4bf95c74046c8
d3netxer <tgertin@vt.edu>
2020-10-12 19:21:14 -0400

    2 new implementations and modifying UrbanRaster code

--- infrasap/UrbanRaster.py
+++ infrasap/UrbanRaster.py
@@ -22,6 +22,66 @@ from shapely.geometry import shape, Polygon
 '''prints the time along with the message'''
 def tPrint(s):
     print("%s\t%s" % (time.strftime("%H:%M:%S"), s))
+    
+def calc_areagrid(inRaster):
+    """
+    # determine area of cells in unprojected raster
+    # from https://gis.stackexchange.com/questions/317392/determine-area-of-cell-in-raster-qgis
+    Create urban definitions using gridded population data.
+        
+    :param inRaster: string or rasterio object representing gridded population data 
+    :returns: A Numpy ndarray of the same dimensions of the input raster where output units are in square km.
+    """
+
+    import numpy.matlib
+
+    if type(inRaster) == str:
+        input_raster = rasterio.open(inRaster)
+    elif isinstance(inRaster, rasterio.DatasetReader):
+        input_raster = inRaster
+        # print(input_raster.closed)
+        if input_raster.closed == True:
+            print('dataset is closed, try to re-open raster')
+    else:
+        raise(ValueError("Input raster dataset must be a file path or a rasterio object"))
+
+    with input_raster as testif:
+        rast = testif.read(1)
+        gt = testif.transform
+        pix_width = gt[0]
+        ulX = gt[2]
+        ulY = gt[5]
+        rows = testif.height
+        cols = testif.width
+        lrX = ulX + gt[0] * cols
+        lrY = ulY + gt[4] * rows
+
+    lats = np.linspace(ulY,lrY,rows+1)
+
+    a = 6378137
+    b = 6356752.3142
+
+    # Degrees to radians
+    lats = lats * np.pi/180
+
+    # Intermediate vars
+    e = np.sqrt(1-(b/a)**2)
+    sinlats = np.sin(lats)
+    zm = 1 - e * sinlats
+    zp = 1 + e * sinlats
+
+    # Distance between meridians
+    #        q = np.diff(longs)/360
+    q = pix_width/360
+
+    # Compute areas for each latitude in square km
+    areas_to_equator = np.pi * b**2 * ((2*np.arctanh(e*sinlats) / (2*e) + sinlats / (zp*zm))) / 10**6
+    areas_between_lats = np.diff(areas_to_equator)
+    areas_cells = np.abs(areas_between_lats) * q
+
+    areagrid = np.transpose(np.matlib.repmat(areas_cells,cols,1))
+
+    return areagrid
 
 class urbanGriddedPop(object):
     def __init__(self, inRaster):
@@ -117,6 +177,7 @@ class urbanGriddedPop(object):
         xxGeom.crs = popRaster.crs
         
         if queen:
+            # res returns the (width, height) of pixels in the units of its coordinate reference system
             xxGeom['geometry '] = xxGeom.buffer((popRaster.res[0] / 2))
             s = xxGeom['geometry']
             overlap_matrix = s.apply(lambda x: s.intersects(x)).values.astype(int)
